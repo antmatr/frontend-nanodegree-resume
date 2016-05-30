ca74507298d3ea60de748021be40b5ef6096358e
Anton Matrosov <antmatr@yandex.ru>
2016-05-30 22:46:13 +0300

    Customize CSS and layout Fix resumeBuilder.js to meet LintJS requiremtns

--- js/helper.js
+++ js/helper.js
@@ -13,47 +13,47 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="text-muted"> // %data%</span>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="text-accent">%contact% </span><span class="text-muted">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="text-muted">mobile </span><span class="text-accent">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="text-muted">email </span><span class="text-accent">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="text-muted">twitter </span><span class="text-accent">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="text-muted">github </span><span class="text-accent">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="text-muted">blog </span><span class="text-accent">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="text-muted">location </span><span class="text-accent">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLwelcomeMsg = '<span class="welcome-message text-muted">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box flex-box-left"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="text-accent border-accent">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkEmployer = '<div class="link-wrap"><a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a></div>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDescription = '<p><br>%data%</p><br>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<div class="link-wrap"><a href="#">%data%</a></div>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' - %data%</a>';
+var HTMLschoolName = '<div class="link-wrap"><a href="#">%data%';
+var HTMLschoolDegree = ' - %data%</a></div>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em><br><br>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineTitle = '<div class="link-wrap"><a href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a></div>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><a href="#">%data%</a><br>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 var bio =
     {
         "name": "Anton Matrosov",
-        "role": "Front-end Developer",
+        "role": "Front-End Developer",
         "contacts": {
             "mobile": "+97253-700-00-00",
             "email": "antmatr@yandex.ru",
@@ -11,36 +11,9 @@ var bio =
         },
         "welcomeMessage": "Lorem Ipsum!",
         "skills": [
-                "HTML", "CSS", "JS", "Git and GitHub", "Web Design"
+                "HTML", "CSS", "JavaScript", "Git and GitHub", "Web Design"
         ],
-        "biopic": "https://s3.amazonaws.com/accredible-api-users/images/122883/original/data?1462825868",
-        "display": function () {
-            $("#header")
-                .prepend(HTMLheaderRole.replace("%data%", bio.role))
-                .prepend(HTMLheaderName.replace("%data%", bio.name))
-                .append(HTMLbioPic.replace("%data%", bio.biopic))
-                .append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
-
-            $("#topContacts")
-                .append(HTMLmobile.replace("%data%", bio.contacts.mobile))
-                .append(HTMLemail.replace("%data%", bio.contacts.email))
-                .append(HTMLtwitter.replace("%data%", bio.contacts.twitter))
-                .append(HTMLgithub.replace("%data%", bio.contacts.gitHub))
-
-                //.append(HTMLblog.replace("%data%", bio.contacts.blog))
-                // there are "HTMLblog" element in helper.js,
-                // but there is no "blog" in bio-object scheme
-
-                .append(HTMLlocation.replace("%data%", bio.contacts.location));
-
-            if (bio.skills.length > 0) {
-                $("#header").append(HTMLskillsStart);
-                for (var i = 0; i < bio.skills.length; i++) {
-                    var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-                    $("#skills").append(formattedSkill);
-                }
-            }
-        }
+        "biopic": "https://s3.amazonaws.com/accredible-api-users/images/122883/original/data?1462825868"
     },
     education =
     {
@@ -66,31 +39,7 @@ var bio =
                     "dates": "2016",
                     "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
                 }
-            ],
-
-        "display": function () {
-            for (var school in education.schools) {
-                if (education.schools.hasOwnProperty(school)) {
-                    $("#education").append(HTMLschoolStart);
-
-                    $(".education-entry:last")
-                        .append(HTMLschoolName.replace("%data%", education.schools[school].name) + HTMLschoolDegree.replace("%data%", education.schools[school].degree))
-                        .append(HTMLschoolDates.replace("%data%", education.schools[school].dates))
-                        .append(HTMLschoolLocation.replace("%data%", education.schools[school].location))
-                        .append(HTMLschoolMajor.replace("%data%", education.schools[school].majors.join(", ")));
-                }
-            }
-            $("#education").append(HTMLonlineClasses);
-            for (var course in education.onlineCourses) {
-                if (education.onlineCourses.hasOwnProperty(course)) {
-                    $("#education").append(HTMLschoolStart);
-                    $(".education-entry:last")
-                        .append(HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title) + HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school))
-                        .append(HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates))
-                        .append(HTMLonlineURL.replace("%data%", education.onlineCourses[course].url));
-                }
-            }
-        }
+            ]
     },
     work =
     {
@@ -117,27 +66,7 @@ var bio =
                     "dates": "2015 - now",
                     "description": "Email-newsletters, flash-banners"
                 }
-            ],
-        "display": function () {
-            for (var job in work.jobs) {
-                if (work.jobs.hasOwnProperty(job)) {
-                    $("#workExperience").append(HTMLworkStart);
-
-                    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer),
-                        formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title),
-                        formattedEmployerTitle = formattedEmployer + formattedTitle,
-                        formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates),
-                        formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location),
-                        formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-
-                    $(".work-entry:last")
-                        .append(formattedEmployerTitle)
-                        .append(formattedDates)
-                        .append(formattedLocation)
-                        .append(formattedDescription);
-                }
-            }
-        }
+            ]
     },
     projects =
     {
@@ -152,35 +81,112 @@ var bio =
                             "https://s3.amazonaws.com/accredible_api_evidence_items/previews/137807/large/1462894789958?1462894059"
                         ]
                 }
-            ],
-        "display": function () {
-            for (var project in projects.projects) {
-                if (projects.projects.hasOwnProperty(project)) {
-
-                    $("#projects").append(HTMLprojectStart);
-
-                    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title),
-                        formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates),
-                        formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description),
-                        images = projects.projects[project].images;
-
-                    $(".project-entry:last")
-                        .append(formattedTitle)
-                        .append(formattedDates)
-                        .append(formattedDescription);
-
-                    for (var image in images)
-                        if (images.hasOwnProperty(image)) {
-                            $(".project-entry:last").append(HTMLprojectImage.replace("%data%", images[image]));
-                        }
-
-                }
-            }
-        }
-    },
-    addMap = function () {
-        $("#mapDiv").append(googleMap);
+            ]
     };
+
+bio.display = function () {
+    $("#header")
+        .prepend(HTMLheaderName.replace("%data%", bio.name))
+        .append(HTMLbioPic.replace("%data%", bio.biopic))
+        .append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+
+    $("#name").append(HTMLheaderRole.replace("%data%", bio.role));
+
+    $("#topContacts")
+        .append(HTMLmobile.replace("%data%", bio.contacts.mobile))
+        .append(HTMLemail.replace("%data%", bio.contacts.email))
+        .append(HTMLtwitter.replace("%data%", bio.contacts.twitter))
+        .append(HTMLgithub.replace("%data%", bio.contacts.gitHub))
+
+        //.append(HTMLblog.replace("%data%", bio.contacts.blog))
+        // there are "HTMLblog" element in helper.js,
+        // but there is no "blog" in bio-object scheme
+
+        .append(HTMLlocation.replace("%data%", bio.contacts.location));
+
+    if (bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+        for (var i = 0; i < bio.skills.length; i++) {
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+            $("#skills").append(formattedSkill);
+        }
+    }
+};
+
+education.display = function () {
+    for (var school in education.schools) {
+        if (education.schools.hasOwnProperty(school)) {
+            $("#education").append(HTMLschoolStart);
+
+            $(".education-entry:last")
+                .append(HTMLschoolName.replace("%data%", education.schools[school].name) + HTMLschoolDegree.replace("%data%", education.schools[school].degree))
+                .append(HTMLschoolDates.replace("%data%", education.schools[school].dates))
+                .append(HTMLschoolLocation.replace("%data%", education.schools[school].location))
+                .append(HTMLschoolMajor.replace("%data%", education.schools[school].majors.join(", ")));
+        }
+    }
+    $("#education").append(HTMLonlineClasses);
+    for (var course in education.onlineCourses) {
+        if (education.onlineCourses.hasOwnProperty(course)) {
+            $("#education").append(HTMLschoolStart);
+            $(".education-entry:last")
+                .append(HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title) + HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school))
+                .append(HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates))
+                .append(HTMLonlineURL.replace("%data%", education.onlineCourses[course].url));
+        }
+    }
+};
+
+work.display = function () {
+    for (var job in work.jobs) {
+        if (work.jobs.hasOwnProperty(job)) {
+            $("#workExperience").append(HTMLworkStart);
+
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer),
+                formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title),
+                formattedEmployerTitle = formattedEmployer + formattedTitle,
+                formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates),
+                formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location),
+                formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+            $(".work-entry:last")
+                .append(formattedEmployerTitle)
+                .append(formattedDates)
+                .append(formattedLocation)
+                .append(formattedDescription);
+        }
+    }
+};
+
+
+projects.display = function () {
+    for (var project in projects.projects) {
+        if (projects.projects.hasOwnProperty(project)) {
+
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title),
+                formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates),
+                formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description),
+                images = projects.projects[project].images;
+
+            $(".project-entry:last")
+                .append(formattedTitle)
+                .append(formattedDates)
+                .append(formattedDescription);
+
+            for (var image in images)
+                if (images.hasOwnProperty(image)) {
+                    $(".project-entry:last").append(HTMLprojectImage.replace("%data%", images[image]));
+                }
+
+        }
+    }
+};
+
+addMap = function () {
+    $("#mapDiv").append(googleMap);
+};
 
 bio.display();
 work.display();
